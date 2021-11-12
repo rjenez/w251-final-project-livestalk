fcd737a3fba693ef44f97373b9cf6b313cafbaa4
Ricardo Jenez <rrj@berkeley.edu>
2021-11-12 09:35:45 -0800

    Clean up

--- yolov5/model_profile.sh
+++ yolov5/model_profile.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
-EPOCHS=200
-IMAGE_SIZE=416
+EPOCHS=100
+IMAGE_SIZE=720
 BATCH_SIZE=16
 #BATCH_SIZE=32
 MODEL=yolov5s.pt
 #MODEL=yolov5m.pt
 #MODEL=yolov5l.pt
-MODEL=yolov5x.pt
+#MODEL=yolov5x.pt
 FREEZE=10
 #FREEZE=24
+CONFIDENCE=0.3
+
--- yolov5/train.sh
+++ yolov5/train.sh
@@ -2,5 +2,5 @@
 
 export PYTHONIOENCODING=utf-8
 
-python3 train.py --img $1 --batch $2 --epochs $3 --data /usr/src/app/finalproject/project.yaml --weights $4 --freeze $5 --cache
+python3 train.py --img $1 --batch $2 --epochs $3 --data /usr/src/app/finalproject/project.yaml --project /usr/src/app/finalproject/runs/train  --weights $4 --freeze $5 --cache
 
