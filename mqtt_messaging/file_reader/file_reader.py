464804f556dc91a0b393ccfaf5fb5d0400815e9f
jrday93 <jrday@ischool.berkeley.edu>
2021-11-28 17:08:39 -0800

    test pipeline

--- mqtt_messaging/file_reader/file_reader.py
+++ mqtt_messaging/file_reader/file_reader.py
@@ -55,10 +55,10 @@ class Handler(FileSystemEventHandler):
             with open(event.src_path, 'rb') as f:
                # file_bytes =f.read()
                 bytes_str = base64.b64encode(f.read()).decode('utf-8')
-                print(bytes_str)
+                #print(bytes_str)
                 if bytes_str:
                     print('in')
-                    json_object = {"name" : event.src_path, "bytes" : bytes_str}
+                    json_object = {"name" : event.src_path, "bytes" : "test_string"}
                # pickle_string = str(pickle.dumps(json_object))
                # prit(pickle_string)
                     #self.mqtt_client.publish(LOCAL_MQTT_TOPIC, "test")
