f304531f95d61391364fe797bf315b390af88a94
jrday93 <jrday@ischool.berkeley.edu>
2021-12-08 12:30:10 -0800

    refactor file reader

--- mqtt_messaging/file_reader/file_reader.py
+++ mqtt_messaging/file_reader/file_reader.py
@@ -1,34 +1,36 @@
-import numpy as np
+import base64
 import cv2 as cv
+import json
+import numpy as np
 import paho.mqtt.client as mqtt
+import pickle
 import sys
 import time
-import json
-from watchdog.observers import Observer
-import pickle
-import base64
-from watchdog.events import FileSystemEventHandler
-from identify import identify, parse_opt
 
-LOCAL_MQTT_HOST="mqtt_broker"
-LOCAL_MQTT_PORT=1883
+from identify import identify, parse_opt
+from watchdog.events import FileSystemEventHandler
+from watchdog.observers import Observer
+
+# Set variables for connecting to MQTT
+LOCAL_MQTT_HOST = "mqtt_broker"
+LOCAL_MQTT_PORT = 1883
 LOCAL_MQTT_TOPIC = "topic"
 FILE_DIRECTORY = "/files"
 
+
 class Watcher:
-    DIRECTORY_TO_WATCH = "/path/to/my/directory"
 
     def __init__(self):
         self.observer = Observer()
 
     def run(self):
         print("trying to connect")
-        local_mqttclient = mqtt.Client()
-        #local_mqttclient.on_connect = on_connect_local
-        #local_mqttclient.on_disconnect=on_disconnect_local
-        local_mqttclient.connect(LOCAL_MQTT_HOST, LOCAL_MQTT_PORT, 36000)
-        event_handler = Handler(local_mqttclient)
-        #event_handler = Handler()
+
+        # Create MQTT Client and pass to event handler for sending data
+        # to MQTT
+        local_mqtt_client = mqtt.Client()
+        local_mqtt_client.connect(LOCAL_MQTT_HOST, LOCAL_MQTT_PORT, 36000)
+        event_handler = Handler(local_mqtt_client)
         self.observer.schedule(event_handler, FILE_DIRECTORY, recursive=True)
         self.observer.start()
         try:
@@ -51,54 +53,41 @@ class Handler(FileSystemEventHandler):
             return None
 
         elif event.event_type == 'modified':
-            # Take any action here when a file is first created.
             print("Received modified event - %s." % event.src_path)
-            # with open(event.src_path, 'rb') as f:
-            #    # file_bytes =f.read()
-            #     bytes_str = base64.b64encode(f.read()).decode('utf-8')
-            #     #print(bytes_str)
-            #     if bytes_str:
-            #         print('in')
-            #         base_file_name = event.src_path.rsplit('/', 1)[-1]
-            #         json_object = {"name" : base_file_name, "bytes" : bytes_str}
-            #         json_string = json.dumps(json_object)
-            #    # pickle_string = str(pickle.dumps(json_object))
-            #    # prit(pickle_string)
-            #         #self.mqtt_client.publish(LOCAL_MQTT_TOPIC, "test")
-            #         self.mqtt_client.publish(LOCAL_MQTT_TOPIC, json_string)
 
 
         elif event.event_type == 'created':
             # Taken any action here when a file is modified.
             print("Received created event - %s." % event.src_path)
             with open(event.src_path, 'rb') as f:
-                # file_bytes =f.read()
-                bytes_compressed = f.read()
-                bytes_str = base64.b64encode(bytes_compressed).decode('utf-8')
+                # Read original byte data and convert to string
+                bytes = f.read()
+                bytes_str = base64.b64encode(bytes).decode('utf-8')
+
+                # Set arguments for running YOLOV5 object detection
                 opt = parse_opt()
-                opt.weights=['/yolov5/best.pt']
+                opt.weights = ['/yolov5/best.pt']
                 opt.conf_thres = [0.45]
+
+                # Run detection
                 id = identify(**vars(opt))
-                image, labels = id.detect(bytes_compressed, conf_thres=0.45)
+                image, labels = id.detect(bytes, conf_thres=0.45)
                 print(' '.join(labels))
+
+                # Read labeled image and covnert to string
                 annotated_bytes_str = base64.b64encode(image).decode('utf-8')
+
+                # As long as original image is not empty
                 if bytes_str:
                     print('in')
+
+                    # Generate json string and send to MQTT broker
                     base_file_name = event.src_path.rsplit('/', 1)[-1]
-                    json_object = {"name": base_file_name, "bytes": bytes_str, "annotated_bytes":annotated_bytes_str, "count": len(labels)}
+                    json_object = {"name": base_file_name, "bytes": bytes_str, "annotated_bytes": annotated_bytes_str,
+                                   "count": len(labels)}
                     json_string = json.dumps(json_object)
                     self.mqtt_client.publish(LOCAL_MQTT_TOPIC, json_string)
 
-# def on_connect_local(client, userdata, flags, rc):
-#     print("connected to local broker with rc: " + str(rc))
-#
-# def on_disconnect_local(client, userdata, rc):
-#     print("disconnecting reason  "  +str(rc))
 
-#print("trying to connect")
-# local_mqttclient = mqtt.Client()
-# local_mqttclient.on_connect = on_connect_local
-# local_mqttclient.on_disconnect=on_disconnect_local
-# local_mqttclient.connect(LOCAL_MQTT_HOST, LOCAL_MQTT_PORT, 60)
 w = Watcher()
 w.run()
